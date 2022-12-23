460df3903064d9d614bd02585e1855c72a86dbba
iboss <theeraphan_p@cmu.ac.th>
2022-12-23 16:01:22 +0700

    i wanna be a pirate king wow

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -1,13 +1,42 @@
-/*
-"Enter your age: "
-"Enter your height: "
-"Enter your bounty: "
-"Your character = "
-"Zoro"
-"Sanji"
-"Usopp"
-"Chopper"
-"Franky"
-"Brook"
-"Jinbe"
-*/
+#include <iostream>
+using namespace std;
+
+int main(){
+
+int age,tall,head;
+
+    cout << "Enter your age: ";
+    cin >> age;
+
+    if(age <= 25){
+
+    cout << "Enter your height: ";
+    cin >> tall;
+
+        if(tall < 100)
+        cout << "Your character = Chopper";
+        else if(tall < 180)
+        cout << "Your character = Usopp";
+        else{
+            cout << "Enter your bounty: ";
+            cin >> head;
+            if(head > 1.1e9)
+            cout << "Your character = Zoro";
+            else
+            cout << "Your character = Sanji";
+        }
+    }
+    else if(age <= 60){
+
+    cout << "Enter your bounty: ";
+    cin >> head;
+
+        if(head > 5e8)
+        cout << "Your character = Jinbe";
+        else
+        cout << "Your character = Franky";
+    }
+    else
+    cout << "Your character = Brook";
+return 0;
+}
